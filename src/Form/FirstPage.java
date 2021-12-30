3f06b27c2a2eca605abdf040f730d15e1f862280
Moahmmed Bakroon <fls3@live.com>
2021-12-30 19:59:52 +0100

    update TheFIrsPage

--- src/Form/FirstPage.java
+++ src/Form/FirstPage.java
@@ -2,11 +2,6 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-<<<<<<< Updated upstream
- * Stefan will make some changes sadad
-=======
- * Stefan will make some changes Hier adsasdad
->>>>>>> Stashed changes
  */
 package Form;
 

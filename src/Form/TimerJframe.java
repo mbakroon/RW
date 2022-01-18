b36ce0f49876b55510734fb5222d41a7d98671ba
Moahmmed Bakroon <fls3@live.com>
2022-01-18 19:24:50 +0200

    Update TimerFrame
    Add Bitcoin Icon

--- src/Form/TimerJframe.java
+++ src/Form/TimerJframe.java
@@ -263,14 +263,19 @@ public class TimerJframe extends javax.swing.JFrame {
         jLabel_seconds = new javax.swing.JLabel();
         jLabel_minute = new javax.swing.JLabel();
         jLabel_milisecond = new javax.swing.JLabel();
-        StartTimer_Button = new javax.swing.JLabel();
         skipTimer_Button = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel16 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jEditorPane1 = new javax.swing.JEditorPane();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
         Background_TimerPanel = new javax.swing.JLabel();
         Hack_background = new javax.swing.JLabel();
 
@@ -318,7 +323,7 @@ public class TimerJframe extends javax.swing.JFrame {
                 Submit_ButtonActionPerformed(evt);
             }
         });
-        jPanel_TimerPanel.add(Submit_Button, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 370, 90, 40));
+        jPanel_TimerPanel.add(Submit_Button, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 560, 100, 40));
 
         jTextField_decryting.setBackground(new java.awt.Color(204, 204, 204));
         jTextField_decryting.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -329,7 +334,7 @@ public class TimerJframe extends javax.swing.JFrame {
                 jTextField_decrytingActionPerformed(evt);
             }
         });
-        jPanel_TimerPanel.add(jTextField_decryting, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 310, 290, 40));
+        jPanel_TimerPanel.add(jTextField_decryting, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 520, 290, 40));
 
         Back.setText("Back");
         Back.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -342,7 +347,7 @@ public class TimerJframe extends javax.swing.JFrame {
                 BackActionPerformed(evt);
             }
         });
-        jPanel_TimerPanel.add(Back, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 20, -1, -1));
+        jPanel_TimerPanel.add(Back, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         jPanel_timerBox.setBackground(new java.awt.Color(105, 111, 113));
         jPanel_timerBox.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
@@ -369,8 +374,6 @@ public class TimerJframe extends javax.swing.JFrame {
         jLabel_milisecond.setFont(new java.awt.Font("Lucida Grande", 0, 24)); // NOI18N
         jLabel_milisecond.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 
-        StartTimer_Button.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/icons8-anfang-48.png"))); // NOI18N
-
         skipTimer_Button.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/icons8-überspringen-50.png"))); // NOI18N
         skipTimer_Button.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         skipTimer_Button.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -391,54 +394,72 @@ public class TimerJframe extends javax.swing.JFrame {
         jLabel16.setForeground(new java.awt.Color(204, 204, 204));
         jLabel16.setText("Second");
 
+        jLabel4.setFont(new java.awt.Font("Lucida Grande", 0, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 0));
+        jLabel4.setText("dein Datei werden be ");
+
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("uebrige Zeit");
+
         javax.swing.GroupLayout jPanel_timerBoxLayout = new javax.swing.GroupLayout(jPanel_timerBox);
         jPanel_timerBox.setLayout(jPanel_timerBoxLayout);
         jPanel_timerBoxLayout.setHorizontalGroup(
             jPanel_timerBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel_timerBoxLayout.createSequentialGroup()
-                .addGap(31, 31, 31)
-                .addGroup(jPanel_timerBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(jPanel_timerBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel_timerBoxLayout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel_timerBoxLayout.createSequentialGroup()
+                        .addGap(31, 31, 31)
                         .addComponent(jLabel_hour)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel_minute)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel2)
-                        .addGap(12, 12, 12)
-                        .addComponent(jLabel_seconds)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel16))
-                    .addGroup(jPanel_timerBoxLayout.createSequentialGroup()
-                        .addComponent(StartTimer_Button)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(skipTimer_Button)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel_timerBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel6)
+                            .addGroup(jPanel_timerBoxLayout.createSequentialGroup()
+                                .addComponent(jLabel_minute)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(jPanel_timerBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(skipTimer_Button)
+                                    .addGroup(jPanel_timerBoxLayout.createSequentialGroup()
+                                        .addComponent(jLabel2)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(jLabel_seconds)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jLabel16)))))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)))
                 .addComponent(jLabel_milisecond, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
         jPanel_timerBoxLayout.setVerticalGroup(
             jPanel_timerBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel_timerBoxLayout.createSequentialGroup()
-                .addGap(40, 40, 40)
+                .addContainerGap()
+                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel_timerBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel_hour)
-                    .addGroup(jPanel_timerBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jLabel_seconds)
+                    .addGroup(jPanel_timerBoxLayout.createSequentialGroup()
                         .addComponent(jLabel_milisecond, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel16)
-                        .addComponent(jLabel2)
-                        .addComponent(jLabel5)
-                        .addComponent(jLabel_minute)))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel_timerBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(skipTimer_Button)
-                    .addComponent(StartTimer_Button))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
+                        .addComponent(skipTimer_Button))
+                    .addGroup(jPanel_timerBoxLayout.createSequentialGroup()
+                        .addComponent(jLabel6)
+                        .addGap(10, 10, 10)
+                        .addGroup(jPanel_timerBoxLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel_hour)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel_minute)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel_seconds)
+                            .addComponent(jLabel16))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
 
-        jPanel_TimerPanel.add(jPanel_timerBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, 290, 150));
+        jPanel_TimerPanel.add(jPanel_timerBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 240, 290, 170));
 
         String strHTML = "<div style= color:red ; font-size. 40px>Was passiert mit meinem Computer</div>";
         jScrollPane1.setViewportView(jEditorPane1);
@@ -447,14 +468,35 @@ public class TimerJframe extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 370, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 422, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
-        jPanel_TimerPanel.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 60, 370, 450));
+        jPanel_TimerPanel.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 60, 400, 420));
+
+        jLabel1.setFont(new java.awt.Font("Lucida Grande", 1, 14)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Ooops, dein Datei sind verschluesselt!");
+        jPanel_TimerPanel.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 20, 280, 20));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/lock.jpeg"))); // NOI18N
+        jLabel3.setText("jLabel3");
+        jLabel3.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel_TimerPanel.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 40, 280, 170));
+
+        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/bitCoin1 copy.jpg"))); // NOI18N
+        jLabel8.setText("jLabel8");
+        jPanel_TimerPanel.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 510, 90, 90));
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/istock-846011096-2.jpg"))); // NOI18N
+        jLabel7.setText("jLabel7");
+        jLabel7.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel_TimerPanel.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 510, 400, 90));
 
         Background_TimerPanel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/timerbackgroundhack copy.gif"))); // NOI18N
         Background_TimerPanel.setText("jLabel3");
@@ -612,12 +654,17 @@ public class TimerJframe extends javax.swing.JFrame {
     private javax.swing.JLabel Background_TimerPanel;
     private javax.swing.JLabel Hack_background;
     private javax.swing.JPanel MainJPanel;
-    private javax.swing.JLabel StartTimer_Button;
     private javax.swing.JButton Submit_Button;
     private javax.swing.JEditorPane jEditorPane1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     public javax.swing.JLabel jLabel_hour;
     private javax.swing.JLabel jLabel_milisecond;
     public javax.swing.JLabel jLabel_minute;
